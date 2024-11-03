cf00ff5b679acd0dbd9482e63eb0eefdc62e4b7b
liaoyueyue <973044869@qq.com>
2024-11-03 23:29:39 +0800

    remake: Problem

--- rear_end/src/main/java/org/example/myojssm/controller/ProblemController.java
+++ rear_end/src/main/java/org/example/myojssm/controller/ProblemController.java
@@ -2,13 +2,14 @@ package org.example.myojssm.controller;
 
 import jakarta.validation.constraints.NotNull;
 import org.example.myojssm.common.Result;
+import org.example.myojssm.entity.PageBean;
 import org.example.myojssm.entity.Problem;
+import org.example.myojssm.entity.vo.ProblemVo;
 import org.example.myojssm.service.ProblemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,35 +25,55 @@ public class ProblemController {
     private ProblemService problemservice;
 
     @PostMapping("/add")
-    public Result addProblem(@RequestBody @Validated(Problem.Add.class) Problem problem) {
-        System.out.println(problem);
-        return problemservice.addProblem(problem);
+    public Result<Void> addProblem(@RequestBody @Validated(Problem.Add.class) Problem problem) {
+        Boolean isAdd = problemservice.addProblem(problem);
+        if (isAdd) {
+            return Result.success();
+        } else {
+            return Result.error("题目添加错误");
+        }
     }
 
     @DeleteMapping("/delete")
-    public Result deleteProblem(@RequestParam @NotNull int id) {
-        return problemservice.deleteProblem(id);
+    public Result<Void> deleteProblem(@RequestParam @NotNull int id) {
+        Boolean isDel = problemservice.deleteProblem(id);
+        if (isDel) {
+            return Result.success();
+        } else {
+            return Result.error("题目删除错误");
+        }
     }
 
     @PutMapping("/update")
-    public Result updateProblem(@RequestBody @Validated(Problem.Update.class) Problem problem) {
-        return problemservice.updateProblem(problem);
+    public Result<Void> updateProblem(@RequestBody @Validated(Problem.Update.class) Problem problem) {
+        Boolean isUpdate = problemservice.updateProblem(problem);
+        if (isUpdate) {
+            return Result.success();
+        } else {
+            return Result.error("题目更新错误");
+        }
     }
 
     @GetMapping("/list")
-    public Result showProblemListByCollectionOrLevel(@NotNull Integer pageNum, @NotNull Integer pageSize, @RequestParam(required = false) String collectionName, @RequestParam(required = false) String level) {
-        return problemservice.getProblemList(pageNum, pageSize, collectionName, level);
-    }
-
-    @GetMapping("/all")
-    public Result showAllProblem() {
-        List<Problem> problems = problemservice.queryAllProblem();
-        return Result.success(problems);
+    public Result<PageBean<ProblemVo>> showProblemListByCollectionOrLevel(@NotNull Integer pageNum, @NotNull Integer pageSize, @RequestParam(required = false) String collectionName, @RequestParam(required = false) String level) {
+        if (pageNum < 1 || pageSize > 20) {
+            return Result.error();
+        }
+        PageBean<ProblemVo> problemList = problemservice.getProblemList(pageNum, pageSize, collectionName, level);
+        if (problemList != null) {
+            return Result.success(problemList);
+        } else {
+            return Result.error("题目列表获取错误");
+        }
     }
 
     @GetMapping("/detail")
-    public Result showProblemById(@RequestParam @NotNull int id) {
+    public Result<Problem> showProblemById(@RequestParam @NotNull int id) {
         Problem problem = problemservice.queryProblemById(id);
-        return problem != null ? Result.success(problem) : Result.error();
+        if (problem != null) {
+            return Result.success(problem);
+        } else {
+            return Result.error("题目详情获取错误");
+        }
     }
 }
